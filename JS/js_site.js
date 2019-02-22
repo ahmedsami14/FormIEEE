d8c90f6ba1c487730e17baf427099aa145ee9b3d
ahmedsami14 <ahmedsami14h@gmail.com>
2019-02-22 22:07:32 +0200

    sam

---
+++ JS/js_site.js
@@ -0,0 +1,35 @@
+"use strict"
+
+function myFunction() {
+
+    var ch = document.getElementsByName("radio");
+    if(ch[0].checked)
+    {
+        window.open("E:/ahmed/IEEE/Tasks/Form%20IEEE2/Form%20IEEE/Form%20IEEE/hr.html","_self");
+    }
+    else if(ch[1].checked)
+    {
+        window.open("E:/ahmed/IEEE/Tasks/Form%20IEEE2/Form%20IEEE/Form%20IEEE/pr.html","_self");
+    }
+    else if(ch[2].checked)
+    {
+        window.open("E:/ahmed/IEEE/Tasks/Form%20IEEE2/Form%20IEEE/Form%20IEEE/java.html","_self");
+    }
+    else if(ch[3].checked)
+    {
+        window.open("E:/ahmed/IEEE/Tasks/Form%20IEEE2/Form%20IEEE/Form%20IEEE/web.html","_self");
+    }
+    else if(ch[4].checked)
+    {
+        window.open("E:/ahmed/IEEE/Tasks/Form%20IEEE2/Form%20IEEE/Form%20IEEE/social.html","_self");
+    }
+    else if(ch[5].checked)
+    {
+        window.open("E:/ahmed/IEEE/Tasks/Form%20IEEE2/Form%20IEEE/Form%20IEEE/media.html","_self");
+    }
+    else if(ch[6].checked)
+    {
+        window.open("E:/ahmed/IEEE/Tasks/Form%20IEEE2/Form%20IEEE/Form%20IEEE/python.html","_self");
+    }
+
+}
